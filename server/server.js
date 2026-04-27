682784367e4096a4736ceba3977be0354ed4a0f4
sarobidy <hei.sarobidy.6pgmail.com>
2026-04-27 12:20:48 +0300

    feat: add backend unit tests

--- server/server.js
+++ server/server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 
 
+app.get("/", (req, res) => {
+  res.json({ message: "Backend running 🚀" });
+});
+
+
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
