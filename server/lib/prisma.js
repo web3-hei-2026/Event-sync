6a6f953d7b685f709c175d830685a3310ea65e06
nyyanja <nyanja.ramahavatra@gmail>
2026-04-29 07:54:17 +0300

    chore: fix prisma v7 configuration with pg adapter

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,5 +1,8 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
