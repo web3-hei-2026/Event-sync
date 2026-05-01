c71733c6665d0f76f89771f49b27e3c1e2b4a4f0
sarobidy <hei.sarobidy.6pgmail.com>
2026-05-01 11:48:33 +0300

    feat: add implement GET and POST the speakers

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/speakers", require("./src/routes/speaker.routes"));
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend running 🚀" });
 });
-
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
