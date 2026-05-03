9a4d74d884ad8838cfce27a7e33cdbbb743c7af0
miorajulia01 <miorahanitra01.com>
2026-05-03 07:26:58 +0300

    fix: resolve server restart error

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -2,10 +2,17 @@ const { PrismaClient } = require('@prisma/client');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { Pool } = require('pg');
 const path = require('path');
+
 require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
+let prisma;
+
+if (!global.prisma) {
+  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  global.prisma = new PrismaClient({ adapter });
+}
+
+prisma = global.prisma;
 
 module.exports = prisma;
