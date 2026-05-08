560a82670868a82bcd18289eb4098bc4b61dfc1a
nyyanja <nyanja.ramahavatra@gmail>
2026-05-08 23:17:38 +0300

    fix(routes): align API routes with controllers

--- server/src/routes/events.route.js
+++ server/src/routes/events.route.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const { getEvents, getEventById } = require('../controllers/events.controller');
+
+const {
+  getEvents,
+  getEventById,
+  createEvent,
+} = require('../controllers/events.controller');
 
 router.get('/', getEvents);
 router.get('/:id', getEventById);
+router.post('/', createEvent);
 
 module.exports = router;
--- server/src/routes/session.route.js
+++ server/src/routes/session.route.js
@@ -4,10 +4,11 @@ const sessionController = require("../controllers/session.controller");
 
 router.get("/events/:eventId/sessions", sessionController.getSessionsByEvent);
 router.get("/events/:id/schedule", sessionController.getEventSessionSchedule);
-router.get("/sessions/:id", sessionController.getSessionById);
-router.post("/sessions", sessionController.createSession);
-router.put("/sessions/:id", sessionController.updateSession);
-router.delete("/sessions/:id", sessionController.deleteSession);
+
+router.get("/:id", sessionController.getSessionById);
+router.post("/", sessionController.createSession);
+router.put("/:id", sessionController.updateSession);
+router.delete("/:id", sessionController.deleteSession);
 
 router.get("/test", (req, res) => {
   res.json({ ok: true, message: "sessions route working" });
