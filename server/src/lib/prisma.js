a90e7ced096c7ff1800afaa79ccca97aa4aadad3
nyyanja <nyanja.ramahavatra@gmail>
2026-05-08 21:13:50 +0300

    fix: prisma initialization cleanup

--- server/src/lib/prisma.js
+++ server/src/lib/prisma.js
@@ -1,18 +1,5 @@
-const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
-const path = require('path');
+const { PrismaClient } = require("@prisma/client");
 
-require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
-
-let prisma;
-
-if (!global.prisma) {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaPg(pool);
-  global.prisma = new PrismaClient({ adapter });
-}
-
-prisma = global.prisma;
+const prisma = new PrismaClient();
 
 module.exports = prisma;
