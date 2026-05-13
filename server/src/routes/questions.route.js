0503bafe028bc09e066fb28a2aa397638efb13cf
Mitia Jessica <mitiah.jessica@gmail.com>
2026-05-13 16:12:16 +0300

    feat: implement question management system with voting, polling, and UI components

--- server/src/routes/questions.route.js
+++ server/src/routes/questions.route.js
@@ -2,12 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { getQuestionsBySessionId, createQuestion, upvoteQuestion, unvoteQuestion } = require('../controllers/questions.controller');
 
-router.get('/sessions/:id/questions', getQuestionsBySessionId);
-
-router.post('/sessions/:id/questions', createQuestion);
-
-router.post('/questions/:id/upvote', upvoteQuestion);
-
-router.post('/questions/:id/unvote', unvoteQuestion);
+router.post('/:id/upvote', upvoteQuestion);
+router.post('/:id/unvote', unvoteQuestion);
 
 module.exports = router;
--- server/src/routes/session.route.js
+++ server/src/routes/session.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const sessionController = require("../controllers/session.controller");
+const questionsController = require("../controllers/questions.controller");
 
 router.get("/:id", sessionController.getSessionById);
 router.post("/", sessionController.createSession);
 router.put("/:id", sessionController.updateSession);
 router.delete("/:id", sessionController.deleteSession);
+
+// Questions for a session
+router.get("/:id/questions", questionsController.getQuestionsBySessionId);
+router.post("/:id/questions", questionsController.createQuestion);
 
 router.get("/test", (req, res) => {
   res.json({ ok: true, message: "sessions route working" });
